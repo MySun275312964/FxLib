8f7ece9d615c0907772a9e75ed1582d958b3c05c
724789975 <724789975@qq.com>
2016-11-29 22:47:14 +0800

    同步

--- Net/net.cpp
+++ Net/net.cpp
@@ -163,7 +163,6 @@ SOCKET FxNetModule::Connect(FxSession* poSession, UINT32 dwIP, UINT16 wPort, boo
 	FxConnection* poConnection = FxConnectionMgr::Instance()->Create();
 	if (NULL == poConnection)
 	{
-		//FxMySockMgr::Instance()->Release(poSock);
 		return INVALID_SOCKET;
 	}
 
